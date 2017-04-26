99e77a007a38648f9b8affa2c9c92514e19d7769
Craig Scott <craigtscottjr@gmail.com>
2017-04-26 14:08:58 -0700

    update user modle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
-  validates :username, :password, presence: true
+  validates :username,
+            :name,
+            :email,
+            :password_digest,
+            :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
 
   attr_reader :password
-  after_initialize :ensure_session_token
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
