151fac29ee5f2ddfd439a7f6ae122c2e7139f25c
Craig Scott <craigtscottjr@gmail.com>
2017-04-27 10:31:03 -0700

    commiting to push

---
+++ app/controllers/api/checkpoints_controller.rb
@@ -0,0 +1,2 @@
+class Api::CheckpointsController < ApplicationController
+end
---
+++ test/controllers/api/checkpoint_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::CheckpointControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
