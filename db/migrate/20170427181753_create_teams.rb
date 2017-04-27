6faf76e9557a7601e36f5a6e500b59b2713add2a
Andrea del Rio <adelrio@gmail.com>
2017-04-27 11:18:49 -0700

    add teams

---
+++ db/migrate/20170427181753_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration[5.0]
+  def change
+    create_table :teams do |t|
+      t.string :name, null: false
+      t.string :logo
+
+      t.timestamps
+    end
+    add_index :teams, :name
+  end
+end
