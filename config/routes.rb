a28bad20af2aeca5462cad139e6e8504103beb38
Andrea del Rio <adelrio@gmail.com>
2017-05-01 15:42:32 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    get 'tag/index'
+  end
+
+  root "static_pages#root"
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show]
+    resources :teams, only: [:index, :create, :update, :show, :destroy]
+    resources :races, only: [:create, :destroy, :index]
+    resource :session, only: [:create, :destroy]
+  end
+
 end
